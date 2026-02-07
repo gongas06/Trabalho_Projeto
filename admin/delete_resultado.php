9f486ff5740b80b0360ba9acb3a2ee7347400582
Gongas06 <goncaloferreira@pvc.pt>
2026-02-07 22:00:40 +0000

    Versão mais Recente

--- admin/delete_resultado.php
+++ admin/delete_resultado.php
@@ -3,6 +3,10 @@
 require_once 'auth.php';
 require_once 'db.php';
 require_login();
+if (!is_admin()) {
+    http_response_code(403);
+    die("Acesso negado. Apenas o administrador pode gerir os resultados.");
+}
 
 $id = intval($_GET['id']);
 $mysqli->query("DELETE FROM resultados WHERE id = $id");
